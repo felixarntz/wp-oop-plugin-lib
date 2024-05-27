bbeaadf681a39d0fac55099ac475bac5aa24e0f4
Felix Arntz <flixos90@gmail.com>
2024-05-27 14:40:43 -0700

    Implement infrastructure for WordPress dependency APIs (scripts, styles, script modules).

---
+++ src/Contracts/Dependency_Registry.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Interface Felix_Arntz\WP_OOP_Plugin_Lib\Contracts\Dependency_Registry
+ *
+ * @since n.e.x.t
+ * @package wp-oop-plugin-lib
+ */
+
+namespace Felix_Arntz\WP_OOP_Plugin_Lib\Contracts;
+
+/**
+ * Interface for a registry of dependencies which allow enqueuing.
+ *
+ * @since n.e.x.t
+ */
+interface Dependency_Registry extends Registry {
+
+	/**
+	 * Enqueues the dependency with the given key.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param string $key Dependency key.
+	 */
+	public function enqueue( string $key ): void;
+
+	/**
+	 * Dequeues the dependency with the given key.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param string $key Dependency key.
+	 */
+	public function dequeue( string $key ): void;
+
+	/**
+	 * Checks whether the dependency with the given key is enqueued.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param string $key Dependency key.
+	 * @return bool True if the dependency is enqueued, false otherwise.
+	 */
+	public function is_enqueued( string $key ): bool;
+}
---
+++ src/Dependencies/Abstract_Dependency_Registry.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Class Felix_Arntz\WP_OOP_Plugin_Lib\Dependencies\Abstract_Dependency_Registry
+ *
+ * @since n.e.x.t
+ * @package wp-oop-plugin-lib
+ */
+
+namespace Felix_Arntz\WP_OOP_Plugin_Lib\Dependencies;
+
+use Felix_Arntz\WP_OOP_Plugin_Lib\Contracts\Dependency_Registry;
+
+/**
+ * Base class for a registry of dependencies, including support for providing manifest files.
+ *
+ * @since n.e.x.t
+ */
+abstract class Abstract_Dependency_Registry implements Dependency_Registry {
+
+	/**
+	 * Parses the given dependency arguments with relevant defaults.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param array<string, mixed> $args Dependency registration arguments.
+	 * @return array<string, mixed> Parsed dependency registration arguments.
+	 */
+	final protected function parse_args( array $args ): array {
+		// Support more verbose 'dependencies' and 'version' keys as an alternative.
+		if ( isset( $args['dependencies'] ) && ! isset( $args['deps'] ) ) {
+			$args['deps'] = $args['dependencies'];
+			unset( $args['dependencies'] );
+		}
+		if ( isset( $args['version'] ) && ! isset( $args['ver'] ) ) {
+			$args['ver'] = $args['version'];
+			unset( $args['version'] );
+		}
+
+		if ( isset( $args['manifest'] ) ) {
+			$args = $this->parse_manifest_args( $args['manifest'], $args );
+			unset( $args['manifest'] );
+		}
+
+		return wp_parse_args(
+			$args,
+			array_merge(
+				$this->get_additional_args_defaults(),
+				array(
+					'src'  => false,
+					'deps' => array(),
+				)
+			)
+		);
+	}
+
+	/**
+	 * Returns defaults to parse dependency arguments with.
+	 *
+	 * The keys 'src' and 'deps' do not need to be included as they are universal defaults for any dependency type.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @return array<string, mixed> Dependency registration defaults.
+	 */
+	abstract protected function get_additional_args_defaults(): array;
+
+	/**
+	 * Parses the given dependency arguments with arguments returned by the given manifest PHP file.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @param string               $manifest Full path of a PHP file which returns arguments for the dependency.
+	 * @param array<string, mixed> $args     Dependency registration arguments.
+	 * @return array<string, mixed> Parsed dependency registration arguments.
+	 */
+	private function parse_manifest_args( string $manifest, array $args ): array {
+		$manifest_args = require $manifest;
+
+		if ( ! is_array( $manifest_args ) ) {
+			_doing_it_wrong( __METHOD__, esc_html__( 'Dependency manifest PHP file must return an array.', 'wp-oop-plugin-lib' ), '' );
+			return $args;
+		}
+
+		return wp_parse_args( $args, $manifest_args );
+	}
+}
