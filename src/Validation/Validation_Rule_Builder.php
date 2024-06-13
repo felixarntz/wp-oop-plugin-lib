7f30c88b6e8b57a4fa7689d501a166fae18ee603
Felix Arntz <flixos90@gmail.com>
2024-06-12 19:45:46 -0700

    Implement ability to get WordPress specific sanitization and validation callbacks via Validation_Rule_Builder.

--- src/Validation/Validation_Rule_Builder.php
+++ src/Validation/Validation_Rule_Builder.php
@@ -9,7 +9,9 @@
 namespace Felix_Arntz\WP_OOP_Plugin_Lib\Validation;
 
 use Felix_Arntz\WP_OOP_Plugin_Lib\Validation\Contracts\Validation_Rule;
+use Felix_Arntz\WP_OOP_Plugin_Lib\Validation\Exception\Validation_Exception;
 use InvalidArgumentException;
+use WP_Error;
 
 /**
  * Class providing a convenience layer to easily compose an aggregate validation rule out of multiple rules.
@@ -213,6 +215,62 @@ final class Validation_Rule_Builder {
 	 */
 	public function get(): Validation_Rule {
 		return new Aggregate_Validation_Rule( $this->rules );
+	}
+
+	/**
+	 * Returns a WordPress option 'sanitize_callback' consisting of all rules present in the builder.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @return callable Callback function to register as an option 'sanitize_callback'.
+	 */
+	public function get_option_sanitize_callback(): callable {
+		$aggregate = $this->get();
+
+		return function ( $value ) use ( $aggregate ) {
+			return $aggregate->sanitize( $value );
+		};
+	}
+
+	/**
+	 * Returns a WordPress REST API 'sanitize_callback' consisting of all rules present in the builder.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @return callable Callback function to register as an REST API 'sanitize_callback'.
+	 */
+	public function get_rest_sanitize_callback(): callable {
+		// For now, this callback looks the same as for sanitizing an option.
+		return $this->get_option_sanitize_callback();
+	}
+
+	/**
+	 * Returns a WordPress REST API 'validate_callback' consisting of all rules present in the builder.
+	 *
+	 * @since n.e.x.t
+	 *
+	 * @return callable Callback function to register as an REST API 'validate_callback'.
+	 */
+	public function get_rest_validate_callback(): callable {
+		$aggregate = $this->get();
+
+		return function ( $value, $request, $param ) use ( $aggregate ) {
+			try {
+				$aggregate->validate( $value );
+			} catch ( Validation_Exception $e ) {
+				return new WP_Error(
+					$e->get_error_code(),
+					sprintf(
+						/* translators: 1: param name, 2: validation exception message */
+						esc_html__( 'Validation for %1$s failed: %2$s', 'wp-oop-plugin-lib' ),
+						esc_html( $param ),
+						$e->getMessage()
+					),
+					array( 'param' => $param )
+				);
+			}
+			return true;
+		};
 	}
 
 	/**
