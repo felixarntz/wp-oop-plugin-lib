2bc47ed79fc0f269b00354c29d62f5199393231a
Felix Arntz <flixos90@gmail.com>
2024-07-11 21:54:29 +0200

    Add test coverage for new capability filter callbacks.

--- tests/phpunit/tests/Capabilities/Capability_Controller_Tests.php
+++ tests/phpunit/tests/Capabilities/Capability_Controller_Tests.php
@@ -14,43 +14,61 @@ use Felix_Arntz\WP_OOP_Plugin_Lib\Capabilities\Capability_Controller;
 use Felix_Arntz\WP_OOP_Plugin_Lib\Capabilities\Meta_Capability;
 use Felix_Arntz\WP_OOP_Plugin_Lib\General\Exception\Not_Found_Exception;
 use Felix_Arntz\WP_OOP_Plugin_Lib\PHPUnit\Includes\Test_Case;
+use WP_UnitTest_Factory;
 
 class Capability_Controller_Tests extends Test_Case {
 
+	private static $admin_id;
+
+	private $container;
 	private $controller;
+
+	public static function wpSetUpBeforeClass( WP_UnitTest_Factory $factory ) {
+		self::$admin_id = $factory->user->create( array( 'role' => 'administrator' ) );
+	}
+
+	public static function wpTearDownAfterClass() {
+		if ( is_multisite() ) {
+			wpmu_delete_user( self::$admin_id );
+		} else {
+			wp_delete_user( self::$admin_id );
+		}
+	}
 
 	public function set_up() {
 		parent::set_up();
 
-		$container = new Capability_Container();
-		$container->set(
+		wp_set_current_user( self::$admin_id );
+
+		$this->container = new Capability_Container();
+		$this->container->set(
 			'with_one_required_cap',
 			static function () {
 				return new Base_Capability( 'with_one_required_cap', array( 'manage_options' ) );
 			}
 		);
-		$container->set(
+		$this->container->set(
 			'with_multiple_required_caps',
 			static function () {
 				return new Base_Capability( 'with_multiple_required_caps', array( 'edit_posts', 'upload_files' ) );
 			}
 		);
-		$container->set(
+		$this->container->set(
 			'without_required_caps',
 			static function () {
 				return new Base_Capability( 'without_required_caps', array() );
 			}
 		);
-		$container->set(
+		$this->container->set(
 			'meta_cap',
 			static function () {
 				return new Meta_Capability( 'meta_cap', static function () {
-					return array( 'with_multiple_required_caps' );
+					return array( 'manage_options' );
 				} );
 			}
 		);
 
-		$this->controller = new Capability_Controller( $container );
+		$this->controller = new Capability_Controller( $this->container );
 	}
 
 	/**
@@ -202,9 +220,43 @@ class Capability_Controller_Tests extends Test_Case {
 		}
 		$this->assertSameSetsWithIndex(
 			array(
-				'meta_cap' => array( 'with_multiple_required_caps' ),
+				'meta_cap' => array( 'manage_options' ),
 			),
 			$resolved_map
 		);
+	}
+
+	public function test_filter_user_has_cap() {
+		add_filter( 'user_has_cap', array( $this->controller, 'filter_user_has_cap' ) );
+
+		$this->assertTrue( current_user_can( 'with_one_required_cap' ) );
+		$this->assertTrue( current_user_can( 'with_multiple_required_caps' ) );
+		$this->assertFalse( current_user_can( 'without_required_caps' ) );
+		$this->assertFalse( current_user_can( 'meta_cap' ) ); // This does not work because the 'map_meta_cap' filter is not applied.
+	}
+
+	public function test_filter_map_meta_cap() {
+		add_filter( 'map_meta_cap', array( $this->controller, 'filter_map_meta_cap' ), 10, 4 );
+
+		$this->assertTrue( current_user_can( 'meta_cap' ) );
+	}
+
+	public function test_filter_map_meta_cap_with_args() {
+		$this->container->set(
+			'meta_cap_with_args',
+			static function () {
+				return new Meta_Capability( 'meta_cap', static function ( int $user_id, int $some_number = 0 ) {
+					if ( 42 === $some_number ) {
+						return array( 'manage_options' );
+					}
+					return array( 'do_not_allow' );
+				} );
+			}
+		);
+
+		add_filter( 'map_meta_cap', array( $this->controller, 'filter_map_meta_cap' ), 10, 4 );
+
+		$this->assertTrue( current_user_can( 'meta_cap_with_args', 42 ) );
+		$this->assertFalse( current_user_can( 'meta_cap_with_args', 41 ) );
 	}
 }
