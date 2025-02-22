86bd8cd88d351898dda5b4d14ceb4c597799f94e
Felix Arntz <flixos90@gmail.com>
2025-02-21 19:41:18 -0800

    Update dependencies.

--- src/Entities/Comment.php
+++ src/Entities/Comment.php
@@ -89,7 +89,6 @@ class Comment implements Entity {
 			return str_replace( '&amp;', '&', (string) get_edit_comment_link( $this->wp_obj ) );
 		}
 
-		// @phpstan-ignore arguments.count
 		return (string) get_edit_comment_link( $this->wp_obj, 'url' );
 	}
 
